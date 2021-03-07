51420a638f38b9a040c0188bdd2b507418a3f0fb
Cam <camglowicki@hotmail.com>
2021-03-06 21:35:02 -0800

    styling for nav and body

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Blog',
+    path: '/blog'
+  },
+  {
+    title: 'Contact Us',
+    path: '/contact-us'
+  },
+  {
+    title: 'Login',
+    path: '/login'
+  }
+]
 
-export default function Nav () {
+export default function Nav() {
   return (
     <nav className="nav-bar">
       <span>Habit</span>
       <ul>
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            {link.title}
+          </li>
+        ))
+
+        }
       </ul>
     </nav>
   );
