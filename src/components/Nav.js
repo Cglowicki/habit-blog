9583811f3e12582793ece46eb6b2367295eac366
Cam <camglowicki@hotmail.com>
2021-03-11 18:24:24 -0800

    add media query for nav dropdown @max-width 900px

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Avatar } from 'antd';
 
@@ -22,10 +22,12 @@ const navLinks = [
 ]
 
 export default function Nav({user}) {
+  const [menuActive, setMenuActive] = useState(false);
+
   return (
     <nav className="nav-bar">
       <span className="menu-title">Habit</span>
-      <div className="menu-content-container">
+      <div className={`menu-content-container ${menuActive && 'active'}`}>
         <ul>
           {navLinks.map((link, index) => (
             <li key={index}>
@@ -38,6 +40,7 @@ export default function Nav({user}) {
         <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38} />
         <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
         </span>
+        <i className="ionicons icon ion-ios-menu" onClick={() => setMenuActive(!menuActive)}/>
       </div>
     </nav>
   );
