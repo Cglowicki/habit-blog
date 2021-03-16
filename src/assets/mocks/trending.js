539a19b6f7635b1edb5169e41f1dba700c782cdb
Cam <camglowicki@hotmail.com>
2021-03-16 11:35:06 -0700

    set up mock data for featured and tredning masonry grids

---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,28 @@
+import moment from 'moment';
+
+export default [
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    link: '#',
+    image: '../images/shabs.jpeg'
+  }
+]
