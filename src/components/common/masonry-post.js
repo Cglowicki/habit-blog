2c4cdca347336aaeff27c53fcaaa84058f4b9a8a
Cam <camglowicki@hotmail.com>
2021-03-17 08:34:09 -0700

    style grid for masonry

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default function MasonryPost ({post, tagsOnTop}) {
+export default function MasonryPost({ post, tagsOnTop }) {
+  const style = {backgroundImage: `url("${require(`../../assets/images/shabs.jpeg`)}")`}
+
   return (
-    <div></div>
+    <a className="masonry-post overlay" style={style} href={post.link}>
+      <div className="image-text"></div>
+    </a>
   )
 }
