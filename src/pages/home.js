8f4d075b4d56b7c9fa6ce48e8c3bf2c84619481d
Cam <camglowicki@hotmail.com>
2021-03-19 14:55:49 -0700

    style elements within trending containers

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { PostMasonry } from '../components/common';
 import trending from '../assets/mocks/trending';
+import featured from '../assets/mocks/featured';
 
 const trendingConfig = {
   1: {
     gridArea: '1 / 2 / 3 / 3'
   }
-}
+};
+
+const featuredConfig = {
+
+};
 
 //merge styles with post object from backend
 const mergeStyles = function (posts, config) {
   posts.forEach((post, index) => {
     post.style = config[index];
   })
-}
+};
 
 mergeStyles(trending, trendingConfig);
+mergeStyles(featured, featuredConfig);
 
 export default function Home() {
   return (
     <section className="container home">
       <div className="row">
-        <h2>Trending Posts</h2>
+        <section>
+          <h1>Featured Posts</h1>
+          <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+        </section>
+        <h1>Trending Posts</h1>
         <PostMasonry posts={trending} columns={3} />
       </div>
     </section>
