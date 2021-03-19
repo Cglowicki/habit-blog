5b33ca309c3c55f466d7eb62f848769aa24421df
Cam <camglowicki@hotmail.com>
2021-03-19 15:12:35 -0700

    setup featured posts masonry

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PostMasonry } from '../components/common';
+import { MasonryPost, PostMasonry } from '../components/common';
 import trending from '../assets/mocks/trending';
 import featured from '../assets/mocks/featured';
 
@@ -10,7 +10,18 @@ const trendingConfig = {
 };
 
 const featuredConfig = {
-
+  0: {
+    gridArea: '1 / 1 / 2 / 3',
+    height: '300px'
+  },
+  1: {
+    height: '300px',
+  },
+  3: {
+    height: '630px',
+    marginLeft: '30px',
+    width: '630px'
+  }
 };
 
 //merge styles with post object from backend
@@ -23,13 +34,16 @@ const mergeStyles = function (posts, config) {
 mergeStyles(trending, trendingConfig);
 mergeStyles(featured, featuredConfig);
 
+const lastFeatured = featured.pop();
+
 export default function Home() {
   return (
     <section className="container home">
       <div className="row">
-        <section>
-          <h1>Featured Posts</h1>
+        <h1>Featured Posts</h1>
+        <section className="featured-posts-container">
           <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+          <MasonryPost post={lastFeatured} tagsOnTop={true} />
         </section>
         <h1>Trending Posts</h1>
         <PostMasonry posts={trending} columns={3} />
