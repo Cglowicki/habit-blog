1c1ded9254503a35f3696cc5d73b35f54dda4a7e
Cam <camglowicki@hotmail.com>
2021-03-22 13:22:31 -0700

    create backend posts folder and copy mocks into it

---
+++ graphql/schemas/posts/mocks/featured.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+import moment from 'moment';
+
+export default [
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Lifestyle', 'Brain Health'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Lifestyle', 'Positivity'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Culture', 'Positivity'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  }
+];
---
+++ graphql/schemas/posts/mocks/trending.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-anonymous-default-export */
+import moment from 'moment';
+
+export default [
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Lifestyle', 'Brain Health'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Lifestyle', 'Positivity'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Culture', 'Positivity'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  },
+  {
+    title:'x',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech', 'Brain Health'],
+    link: '#',
+    image: '../images/shabs.jpeg'
+  }
+];
