60f877a5fdb4834cab9fd6ed67b85ca347a28d69
Cam <camglowicki@hotmail.com>
2021-03-24 13:16:10 -0700

    add script to create-migration w/ tableName

--- graphql/database/actions/create-migration.js
+++ graphql/database/actions/create-migration.js
@@ -9,11 +9,11 @@ try {
   ).split('=')[1];
 } catch (err) {
     return console.error('--tableName parameter not found. Please specify a table name.');
-}
+};
 
 const fileName = new Date().getTime() + `_${tableName}.sql`;
 
 fs.writeFile(path.resolve(__dirname, `../migrations/${fileName}`), '', (err) => {
   if (err) throw new Error(err);
-  console.log('Created new migration file in migrations folder.')
+  console.log('Created new migration file in migrations folder.');
 });
