975ab91e4c821522ad703ab9db3fa1de345a6982
Cam <camglowicki@hotmail.com>
2021-03-29 10:49:13 -0700

    create users table

---
+++ graphql/database/migrations/1616617585599_users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS {}.users (
+  id INT(12) NOT NULL auto_increment PRIMARY KEY,
+  title VARCHAR(20) NOT NULL,
+  role_id INT(12) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  image VARCHAR(200),
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  INDEX uemail (email),
+  CONSTRAINT fk_urole FOREGIN KEY(role_id),
+  REFERENCES roles(id),
+  ON DELETE CASCADE,
+  ON UPDATE CASCADE
+);
